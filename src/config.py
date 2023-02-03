1f7019558aec72c10f0669056e870c81c17de99b
RandomTwins <124370261+RandomTwins@users.noreply.github.com>
2023-02-03 16:36:52 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-content_folder = ['art', 'blog']
+content_folder = ['art', 'blog', 'journal']
 relative_build_url = "/"
 absolute_build_url = "https://randomtwins.github.io/Chisai/"
 build_folder = "build/"
